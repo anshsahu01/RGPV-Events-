c93b833b69f2c601391e14ffdff4a7a16a6d5478
Ansh <anshsahu7705@gmail.com>
2025-10-17 10:03:12 +0530

    Cors issue

--- Client/src/api/axiosInstance.js
+++ Client/src/api/axiosInstance.js
@@ -4,7 +4,7 @@ import { setTokens } from "../redux/authSlice";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
-  withCredentials: true, // allow sending cookies
+  // withCredentials: true, 
 });
 
 
